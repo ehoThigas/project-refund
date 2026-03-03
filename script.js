c95a16d367210e2482151c11e737ed9e707ae847
Thiago Lucas <thiago.lucasc@outlook.com>
2026-03-03 20:23:39 -0300

    Finish project

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const form = document.querySelector('form');
 const expense = document.querySelector('#expense');
 const category = document.querySelector('#category');
 const amount = document.querySelector('#amount');
-const expenseHeader = document.querySelector('header');
+const expenseQuantity = document.querySelector('aside header p span');
+const expenseTotal = document.querySelector('aside header h2');
 const expenseList = document.querySelector('ul');
-let expensesNumber = 0;
 
 amount.addEventListener("input", () => {
     let value = amount.value.replace(/\D/g, "");
-    value = Number(value) / 100
+    value = Number(value) / 100;
 
     amount.value = convertCurrencyBRL(value);
 })
@@ -22,6 +22,7 @@ form.addEventListener("submit", (e) => {
 expenseList.addEventListener("click", (e) => {
     if(e.target.classList.contains("remove-icon")){
         e.target.closest("li").remove();
+        updateTotal();
     }
 })
 
@@ -62,25 +63,48 @@ function addExpense(newExpense){
 
                 <img src="./img/remove.svg" alt="remover" class="remove-icon" />
             </li>
-        `
+        `;
         
         expenseList.insertAdjacentHTML("beforeend", expenseItem);
+        updateTotal();
+        
+        form.reset();
+        expense.focus();
     } catch (error) {
-        alert("Não foi possível atualizar a lista de despesas.")
+        alert("Não foi possível atualizar a lista de despesas.");
         console.log(error);
     }
 }
 
-function updateTotal(){
-    
-    
-    newHeader = `
-        <p>
-              Minhas solicitações <i>&bullet;</i>
-              <span>${escapeHTML(expensesNumber)} despesas</span>
-            </p>
-            <h2><small>R$</small>0,00</h2>
-    `
+function updateTotal(){  
+    try{
+        const items = expenseList.querySelectorAll('li');
+        let total = 0;
+
+        expenseQuantity.textContent = `${items.length} ${
+            items.length > 1 ? "despesas" : "despesa"
+        }`;
+
+        for(let item = 0; item < items.length; item++){
+            const itemAmount = items[item].querySelector('.expense-amount');
+
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".");
+            value = parseFloat(value);
+
+            if(isNaN(value)) return alert('Não foi possível calcular o total, o valor não parece ser um número.');
+
+            total += Number(value);
+        }
+
+        total = convertCurrencyBRL(total).replace(/R\$\s?/, "");
+        const newHTML = `<small>R$</small>${escapeHTML(total)}`;
+
+        expenseTotal.innerHTML = '';
+        expenseTotal.insertAdjacentHTML("beforeend", newHTML);
+    } catch(error){
+        alert("Não foi possível atualizar o total.");
+        console.log(error);
+    }
 }
 
 function escapeHTML(str){
